b8dcdd950ccd334cd3fed548006e4b07e8e2d54f
Aleksey Stepin <diver911@yandex.ru>
2021-09-26 00:50:33 +0300

    change 'if'

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,8 +4,11 @@ public class SQRService {
     public int rootOfNumber(int minNumber, int maxNumber) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= minNumber && i * i < maxNumber) {
-                count = count +1;
+            if (i * i >= minNumber) {
+                if (i * i <= maxNumber) {
+                    count = count + 1;
+                }
+
             }
 
         }
